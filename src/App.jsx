5f8cdaf677c1ce2979f93bb690d980bb871da527
mohtashim21 <mohteshim3276@gmail.com>
2024-03-19 16:52:32 +0530

    made changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import Nav from "./components/Navbar";
-import Detail from "./pages/Detail";
 import ViewOrder from "./pages/ViewOrder";
 import Products from "./pages/Products";
-import AddHomeMen from "./pages/AddHomeMen";
-import AddHomeWomen from "./pages/AddHomeWomen";
-import AddMen from "./pages/AddMen";
-import AddWomen from "./pages/AddWomen";
+// import AddHomeMen from "./pages/AddHomeMen";
+// import AddHomeWomen from "./pages/AddHomeWomen";
+// import AddMen from "./pages/AddMen";
+// import AddWomen from "./pages/AddWomen";
 import Categories from "./pages/Categories";
 import MenPage from "./pages/MenPage";
 import WomenPage from "./pages/WomenPage";
@@ -26,15 +25,13 @@ const App = () => {
         <Route path="/products" element={<Products />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/addhomemen" element={<AddHomeMen />} />
+        {/* <Route path="/addhomemen" element={<AddHomeMen />} />
         <Route path="/addhomewomen" element={<AddHomeWomen />} />
         <Route path="/addmen" element={<AddMen />} />
-        <Route path="/addwomen" element={<AddWomen />} />
+        <Route path="/addwomen" element={<AddWomen />} /> */}
         <Route path="/menpage" element={<MenPage />} />
         <Route path="/womenpage" element={<WomenPage />} />
         <Route path="/categories" element={<Categories />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/product/view/:productId" element={<Detail />} />
         <Route path="/orders" element={<ViewOrder />} />
         <Route path="/productdetail/:id" element={<ProductDetail />} />
       </Routes>
